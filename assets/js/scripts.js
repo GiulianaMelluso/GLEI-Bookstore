f2d5e03694ec9c16ca56263629605430b48ed8af
Giuliana Melluso <giulianamelluso@gmail.com>
2021-11-09 23:59:45 -0300

    Arreglo cosas de la tipografia

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -21,10 +21,6 @@ jQuery(document).ready(function() {
 		e.preventDefault();
 		scroll_to($(this), $('nav').outerHeight());
 	});
-	// toggle "navbar-no-bg" class
-	$('.top-content .text').waypoint(function() {
-		$('nav').toggleClass('navbar-no-bg');
-	});
 		
 });
 
